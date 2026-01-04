156871c7bc72d04d1596e660e08d3e951d109d5b
lileeluna <elliechan.nhs@gmail.com>
2026-01-04 01:53:15 -0800

    added first attempt at adding general chores + weekly chores

--- chore_bot.py
+++ chore_bot.py
@@ -18,8 +18,43 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'{bot.user.name} is online!')
 
+CHORES = 'chores.json'
+
+def load_chores():
+    try:
+        with open(CHORES, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+    
+def save_chores(chores):
+    with open(CHORES, 'w') as f:
+        json.dump(chores, f, indent=4)
+
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong!')
+async def add_chore(ctx, user: discord.Member, chore_name: str, frequency_days: int):
+    chores = load_chores()
+    
+    chores[chore_name] = {
+        'user_id': user.id,
+        'frequency_days': frequency_days,
+        'last_done': None
+    }
+
+    save_chores(chores)
+    await ctx.send(f'Chore "{chore_name}" added for {user.mention} with frequency {frequency_days} days.')
+
+@bot.command()
+async def add_weekly_chore(ctx, user: discord.Member, chore_name: str):
+    chores = load_chores()
+    
+    chores[chore_name] = {
+        'user_id': user.id,
+        'frequency_days': 7,
+        'last_done': None
+    }
+
+    save_chores(chores)
+    await ctx.send(f'Weekly chore "{chore_name}" added for {user.mention}.')
 
 bot.run(TOKEN)
