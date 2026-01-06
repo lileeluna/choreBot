31228bf74de26e7b97613339cecbe48f86a7e266
lileeluna <elliechan.nhs@gmail.com>
2026-01-05 16:52:54 -0800

    added function to check for next chore

--- chore_bot.py
+++ chore_bot.py
@@ -10,16 +10,19 @@ from dateutil.relativedelta import relativedelta
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Set up bot intents and command prefix
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+CHORES = 'chores.json'
+
+# Debug message to show bot is online
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is online!')
 
-CHORES = 'chores.json'
-
+# Helper functions to load and save chores
 def load_chores():
     try:
         with open(CHORES, 'r') as f:
@@ -34,6 +37,7 @@ def save_chores(chores):
     with open(CHORES, 'w') as f:
         json.dump(chores, f, indent=4)
 
+# Command to add a chore with custom frequency
 @bot.command()
 async def addchore(ctx, user: discord.Member, chore_name: str, frequency_days: int):
     chores = load_chores()
@@ -47,6 +51,7 @@ async def addchore(ctx, user: discord.Member, chore_name: str, frequency_days: i
     save_chores(chores)
     await ctx.send(f'Chore "{chore_name}" added for {user.mention} with frequency {frequency_days} days.')
 
+# Command to add a weekly chore
 @bot.command()
 async def addweeklychore(ctx, user: discord.Member, chore_name: str):
     chores = load_chores()
@@ -60,6 +65,7 @@ async def addweeklychore(ctx, user: discord.Member, chore_name: str):
     save_chores(chores)
     await ctx.send(f'Weekly chore "{chore_name}" added for {user.mention}.')
 
+# Command to remove a chore
 @bot.command()
 async def removechore(ctx, chore_name: str):
     chores = load_chores()
@@ -71,6 +77,7 @@ async def removechore(ctx, chore_name: str):
     else:
         await ctx.send(f'Chore "{chore_name}" not found.')
 
+# Command to list all chores
 @bot.command()
 async def listchores(ctx):
     chores = load_chores()
@@ -88,6 +95,7 @@ async def listchores(ctx):
 
     await ctx.send(message)
 
+# Command to mark a chore as done
 @bot.command()
 async def donechore(ctx, chore_name: str):
     chores = load_chores()
@@ -98,5 +106,26 @@ async def donechore(ctx, chore_name: str):
         await ctx.send(f'Chore "{chore_name}" marked as done.')
     else:
         await ctx.send(f'Chore "{chore_name}" not found.')
+
+# Command to check next due date for a chore
+async def __nextchore(ctx, chore_name: str):
+    chores = load_chores()
+    
+    if chore_name in chores:
+        last_done_str = chores[chore_name]['last_done']
+        frequency_days = chores[chore_name]['frequency_days']
+        
+        if last_done_str:
+            last_done = datetime.fromisoformat(last_done_str).date()
+            next_due = last_done + relativedelta(days=frequency_days)
+            await ctx.send(f'Next due date for chore "{chore_name}" is {next_due.isoformat()}.')
+        else:
+            await ctx.send(f'Chore "{chore_name}" has never been done. It is due now.')
+    else:
+        await ctx.send(f'Chore "{chore_name}" not found.')
+
+@bot.command()
+async def nextchore(ctx, chore_name: str):
+    await __nextchore(ctx, chore_name)
 
 bot.run(TOKEN)
